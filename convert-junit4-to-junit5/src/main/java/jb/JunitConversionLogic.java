78bcec2417340577582876be144b02bd584f6df6
Monica Bui <mbui@atb.com>
2018-06-25 09:26:46 -0600

    Changed dependencies to execute jar file and changed logic to include mockito also

--- convert-junit4-to-junit5/src/main/java/jb/JunitConversionLogic.java
+++ convert-junit4-to-junit5/src/main/java/jb/JunitConversionLogic.java
@@ -1,82 +1,92 @@
 package jb;
 
-import static jb.RegExHelper.*;
+import static jb.RegExHelper.replaceUnlessFollowedByEscapingPackageName;
+import static jb.RegExHelper.replaceUnlessPreceededBy;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
 
-import com.github.javaparser.*;
-import com.github.javaparser.ast.*;
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
 
 public class JunitConversionLogic {
 
-	private JunitConversionLogic() {
-		super();
-	}
+    private JunitConversionLogic() {
+        super();
+    }
 
-	public static String convert(String originalText) {
-		String result = originalText;
-		// don't update file if already on JUnit 5
-		if (!originalText.contains("org.junit.jupiter")) {
-			// easier to do these with plain text
-			result = convertPackage(result);
-			result = convertAnnotations(result);
-			result = convertClassNames(result);
-			result = addAssertThatImport(result);
+    public static String convert(String originalText) {
+        String result = originalText;
+        // don't update file if already on JUnit 5
+        if (!originalText.contains("org.junit.jupiter")) {
+            // easier to do these with plain text
+            result = convertPackage(result);
+            result = convertAnnotations(result);
+            result = convertClassNames(result);
+            result = addAssertThatImport(result);
 
-			// easier to do move parameter order with AST parser
-			CompilationUnit cu = JavaParser.parse(new ByteArrayInputStream(result.getBytes()));
-			convertAssertionsAndAssumptionMethodParamOrder(cu);
+            System.out.println(result);
 
-			result = cu.toString();
-		}
+            // easier to do move parameter order with AST parser
+            CompilationUnit cu = JavaParser.parse(new ByteArrayInputStream(result.getBytes()));
+            convertAssertionsAndAssumptionMethodParamOrder(cu);
 
-		return result;
-	}
-	
-	private static String convertPackage(String originalText) {
-		String result = originalText;
-		result = result.replaceAll("org.junit.Assert.assertThat", "org.hamcrest.MatcherAssert.assertThat");
-		result = result.replaceAll("org.junit.", "org.junit.jupiter.api.");
-		return result;
-	}
+            result = cu.toString();
+        }
 
-	private static String convertAnnotations(String originalText) {
-		String result = originalText;
-		result = result.replace("org.junit.jupiter.api.BeforeClass", "org.junit.jupiter.api.BeforeAll");
-		result = replaceUnlessFollowedByEscapingPackageName(result, "org.junit.jupiter.api.Before", "All",
-				"org.junit.jupiter.api.BeforeEach");
-		result = result.replace("org.junit.jupiter.api.AfterClass", "org.junit.jupiter.api.AfterAll");
-		result = replaceUnlessFollowedByEscapingPackageName(result, "org.junit.jupiter.api.After", "All",
-				"org.junit.jupiter.api.AfterEach");
-		result = result.replace("org.junit.jupiter.api.Ignore", "org.junit.jupiter.api.Disabled");
-		result = result.replace("@BeforeClass", "@BeforeAll");
-		result = replaceUnlessFollowedByEscapingPackageName(result, "@Before", "All", "@BeforeEach");
-		result = result.replace("@AfterClass", "@AfterAll");
-		result = replaceUnlessFollowedByEscapingPackageName(result, "@After", "All", "@AfterEach");
-		return result.replace("@Ignore", "@Disabled");
-	}
+        return result;
+    }
 
-	private static String convertClassNames(String originalText) {
-		String result = originalText;
-		result = result.replace("org.junit.jupiter.api.Assert", "org.junit.jupiter.api.Assertions");
-		result = result.replace("org.junit.jupiter.api.Assume", "org.junit.jupiter.api.Assumptions");
-		// don't update for hamcrest "MatcherAssert"
-		result = replaceUnlessPreceededBy(result, "Assert.assert", "Matcher", "Assertions.assert");
-		result = result.replace("Assert.fail", "Assertions.fail");
-		result = result.replace("Assume.assume", "Assumptions.assume");
-		return result;
-	}
+    private static String convertPackage(String originalText) {
+        String result = originalText;
+        result = result.replaceAll("org.junit.Assert.assertThat", "org.hamcrest.MatcherAssert.assertThat");
+        result = result.replaceAll("org.junit.", "org.junit.jupiter.api.");
+        return result;
+    }
 
-	// Assert that moved from junit core to hamcrest matchers
-	private static String addAssertThatImport(String originalText) {
-		String result = originalText;
-		if (originalText.contains("assertThat") && !originalText.contains("org.hamcrest.MatcherAssert")) {
-			result = result.replaceFirst("import", "import static org.hamcrest.MatcherAssert.assertThat;\nimport");
-		}
-		return result;
-	}
+    private static String convertAnnotations(String originalText) {
+        String result = originalText;
+        result = result.replace("org.junit.jupiter.api.BeforeClass", "org.junit.jupiter.api.BeforeAll");
+        result = replaceUnlessFollowedByEscapingPackageName(result, "org.junit.jupiter.api.Before", "All",
+            "org.junit.jupiter.api.BeforeEach");
+        result = result.replace("org.junit.jupiter.api.AfterClass", "org.junit.jupiter.api.AfterAll");
+        result = replaceUnlessFollowedByEscapingPackageName(result, "org.junit.jupiter.api.After", "All",
+            "org.junit.jupiter.api.AfterEach");
+        result = result.replace("org.junit.jupiter.api.Ignore", "org.junit.jupiter.api.Disabled");
+        result = result.replace("@BeforeClass", "@BeforeAll");
+        result = replaceUnlessFollowedByEscapingPackageName(result, "@Before", "All", "@BeforeEach");
+        result = result.replace("@AfterClass", "@AfterAll");
+        result = replaceUnlessFollowedByEscapingPackageName(result, "@After", "All", "@AfterEach");
 
-	private static void convertAssertionsAndAssumptionMethodParamOrder(CompilationUnit cu) {
-		new MoveMessageParameterVisitor().visit(cu, null);
-	}
+        result = result.replace("org.junit.jupiter.api.runner.RunWith", "org.junit.jupiter.api.extension.ExtendWith");
+        result = result.replace("@RunWith(MockitoJUnitRunner.class)",
+            "@ExtendWith(MockitoExtension.class)\n@MockitoSettings(strictness = Strictness.LENIENT)");
+
+        result = result.replace("org.mockito.runners.MockitoJUnitRunner;",
+            "org.mockito.junit.jupiter.MockitoExtension;\nimport org.mockito.junit.jupiter.MockitoSettings;\nimport org.mockito.quality.Strictness;");
+        return result.replace("@Ignore", "@Disabled");
+    }
+
+    private static String convertClassNames(String originalText) {
+        String result = originalText;
+        result = result.replace("org.junit.jupiter.api.Assert", "org.junit.jupiter.api.Assertions");
+        result = result.replace("org.junit.jupiter.api.Assume", "org.junit.jupiter.api.Assumptions");
+        // don't update for hamcrest "MatcherAssert"
+        result = replaceUnlessPreceededBy(result, "Assert.assert", "Matcher", "Assertions.assert");
+        result = result.replace("Assert.fail", "Assertions.fail");
+        result = result.replace("Assume.assume", "Assumptions.assume");
+        return result;
+    }
+
+    // Assert that moved from junit core to hamcrest matchers
+    private static String addAssertThatImport(String originalText) {
+        String result = originalText;
+        if (originalText.contains("assertThat") && !originalText.contains("org.hamcrest.MatcherAssert")) {
+            result = result.replaceFirst("import", "import static org.hamcrest.MatcherAssert.assertThat;\nimport");
+        }
+        return result;
+    }
+
+    private static void convertAssertionsAndAssumptionMethodParamOrder(CompilationUnit cu) {
+        new MoveMessageParameterVisitor().visit(cu, null);
+    }
 }
